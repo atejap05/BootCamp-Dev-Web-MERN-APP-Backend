c003e9123733f76cd0a25ca43a257001539a0599
Juliano Costa <julianohcosta@gmail.com>
2022-12-10 23:06:44 -0400

    Improves server response

--- src/routes/intencao.routes.js
+++ src/routes/intencao.routes.js
@@ -6,6 +6,46 @@ import UnidadeModel from "../models/unidade.model.js";
 
 const IntencaoRouter = express.Router();
 
+
+const populateIntencoes = async (allIntencoes) => {
+
+    const userIds = allIntencoes.map(intent => intent.userId)
+    const unidadesOrigemIds = allIntencoes.map(intent => intent.origemId)
+    const unidadesDestinoIds = allIntencoes.map(intent => intent.destinoId)
+
+    const users = await UserModel.find({ '_id': { $in: userIds } });
+    const unidadesOrigem = await UnidadeModel.find({ '_id': { $in: unidadesOrigemIds } });
+    const unidadesDestino = await UnidadeModel.find({ '_id': { $in: unidadesDestinoIds } });
+
+    const populatedIntents = []
+
+    allIntencoes.forEach(intent => {
+
+        // Get user
+        const user = users.find(user => user._id.toString() === intent.userId.toString())
+
+        delete user['_doc'].passwordHash
+
+        // get Unidade origem.
+        const unidadeOrigem = unidadesOrigem.find(u => u._id.toString() === intent.origemId.toString())
+        //console.log(unidadeOrigem)
+
+        // get Unidade destino.
+        const unidadeDestino = unidadesDestino.find(u => u._id.toString() === intent.destinoId.toString())
+        //console.log(unidadeDestino)
+
+        populatedIntents.push({
+            _id: intent._id,
+            createdAt: intent.createdAt,
+            unidadeOrigem,
+            unidadeDestino,
+            user
+        })
+    } )
+
+    return populatedIntents
+}
+
 IntencaoRouter.post('/create', isAuth, async (req, res) => {
     /* 	#swagger.tags = ['Intencao']
         #swagger.path = '/intencao/create'
@@ -59,39 +99,8 @@ IntencaoRouter.get('/all', isAuth, async (req, res) => {
     try{
 
         const allIntencoes = await IntencaoModel.find()
-        const userIds = allIntencoes.map(intent => intent.userId)
-        const unidadesOrigemIds = allIntencoes.map(intent => intent.origemId)
-        const unidadesDestinoIds = allIntencoes.map(intent => intent.destinoId)
-
-        const users = await UserModel.find({ '_id': { $in: userIds } });
-        const unidadesOrigem = await UnidadeModel.find({ '_id': { $in: unidadesOrigemIds } });
-        const unidadesDestino = await UnidadeModel.find({ '_id': { $in: unidadesDestinoIds } });
-
-        const populatedIntents = []
-
-        allIntencoes.forEach(intent => {
-
-            // Get user
-            const user = users.find(user => user._id.toString() === intent.userId.toString())
-
-            delete user['_doc'].passwordHash
-
-            // get Unidade origem.
-            const unidadeOrigem = unidadesOrigem.find(u => u._id.toString() === intent.origemId.toString())
-            //console.log(unidadeOrigem)
-
-            // get Unidade destino.
-            const unidadeDestino = unidadesDestino.find(u => u._id.toString() === intent.destinoId.toString())
-            //console.log(unidadeDestino)
-
-            populatedIntents.push({
-                unidadeOrigem,
-                unidadeDestino,
-                user
-            })
-        } )
-
-        return res.status(200).json(populatedIntents)
+        const resposta = await populateIntencoes(allIntencoes)
+        return res.status(200).json(resposta)
 
     } catch (error) {
         console.log(error);
@@ -99,7 +108,21 @@ IntencaoRouter.get('/all', isAuth, async (req, res) => {
     }
 });
 
-IntencaoRouter.get('/:id', async (req,res) => {
+IntencaoRouter.get('/byUser/:userId', async (req,res) => {
+    try {
+        const { userId } = req.params;
+        //Verificar como o será passado o usuário logado e sua unidade
+        const allIntencoes = await IntencaoModel.find({userId})
+        const resposta = await populateIntencoes(allIntencoes)
+        return res.status(200).json(resposta)
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error.errors);
+    }
+});
+
+IntencaoRouter.get('/:id', isAuth, async (req,res) => {
     try {
         const { id } = req.params;
         //Verificar como o será passado o usuário logado e sua unidade
